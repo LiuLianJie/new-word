0be0dac27096907c117f8f538a78ed6ece2cbb70
LiuLianJie <lj316641206@163.com>
2015-09-15 02:32:18 +0800

    add loginview

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,20 @@
-var style = {
-	position:'absolute',
-	top:'50%',
-	left:'50%',
-	transform:'translate(-50% , -50%)',
-}
+var LoginForm = React.createClass({
+	render: function(){
+		return (
+			<form className="form-signin">
+		        <h2 className="form-signin-heading">请登录</h2>
+		        <label for="inputEmail" className="sr-only">请输入邮箱</label>
+		        <input type="email" id="inputEmail" className="form-control" placeholder="请输入邮箱" />
+		        <label for="inputPassword" className="sr-only">请输入密码</label>
+		        <input type="password" id="inputPassword" className="form-control" placeholder="请输入密码" />
+
+		        <button className="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+		    </form>
+		);
+	}
+});
+
 React.render(
-	<div style={style}>
-		<h1>小鱼单词 即将到来</h1>
-		<p style={{textAlign:'center'}}> comming soon </p>
-	</div>,
+	<LoginForm />,
 	document.getElementById('container')
 );
