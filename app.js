45e639313c1db3c4d7868279818081f334d92bcd
LiuLianJie <lj316641206@163.com>
2015-09-16 05:11:38 +0800

    add login function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
+var mongoose = require('mongoose');
+
+
 
 var app = express();
 app.set('port',process.env.PORT || 3001);
@@ -9,8 +12,38 @@ app.engine('.html',ejs.__express);
 app.set('view engine','html');
 app.use(express.static(path.join(__dirname,'static')));
 
+var db = mongoose.createConnection('mongodb://127.0.0.1:27017/NodeJS');
+db.on('error',function(error){
+	console.log(error);
+});
+
+var mongooseSchema = new mongoose.Schema({
+    username : {type : String, default : '匿名用户'},
+    title    : {type : String},
+    content  : {type : String},
+    time     : {type : Date, default: Date.now},
+    age      : {type : Number}
+});
+var mongooseModel = db.model('mongoose', mongooseSchema);
+
 app.get('/',function(req,res){
 	res.render('index');
+});
+
+app.get('/login',function(req,res){
+	console.log(req.query);
+	var doc = {username : 'emtity_demo_username', title : 'emtity_demo_title', content : 'emtity_demo_content'};
+	var mongooseEntity = new mongooseModel(doc);
+	mongooseEntity.save(function(error) {
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log('saved OK!');
+	    }
+	    // 关闭数据库链接
+	    db.close();
+	});
+	res.json({'s':1});
 });
 
 app.listen(app.get('port'),function(){
--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,35 @@
 var LoginForm = React.createClass({
+	getInitialState: function(){
+		return {
+			username:'ss',
+			password:'ss'
+		};
+	},
+	loginhandle: function(){
+		var username = React.findDOMNode(this.refs.usernameInput).value;
+		var password = React.findDOMNode(this.refs.passwordInput).value;
+		$.ajax({
+			url:'/login',
+			type:'get',
+			data:{username:username,password:password},
+			dataType:'json',
+			success:function(data){
+				
+			}
+		});
+	},
 	render: function(){
+		var username = this.state.username;
+		var password = this.state.password;
 		return (
 			<form className="form-signin">
 		        <h2 className="form-signin-heading">请登录</h2>
 		        <label for="inputEmail" className="sr-only">请输入邮箱</label>
-		        <input type="email" id="inputEmail" className="form-control" placeholder="请输入邮箱" />
+		        <input ref="usernameInput" type="text" id="inputEmail" className="form-control" placeholder="请输入邮箱"/>
 		        <label for="inputPassword" className="sr-only">请输入密码</label>
-		        <input type="password" id="inputPassword" className="form-control" placeholder="请输入密码" />
+		        <input ref="passwordInput" type="password" id="inputPassword" className="form-control" placeholder="请输入密码"/>
 
-		        <button className="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+		        <button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.loginhandle}>登入</button>
 		    </form>
 		);
 	}
